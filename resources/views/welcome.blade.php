cb23fb7d695ab9714df9a88f1a2caf8b56db3751
migbertweb <migbertyanez@disroot.org>
2024-08-08 14:52:55 -0300

    Modificar el Welcome
    modificar el titulo
    modificar para mostrar la traduccion

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
@@ -27,7 +27,6 @@
                         @endif
                     </header>
 
-                    <main class="mt-6">
                         <div class="grid gap-6 lg:grid-cols-2 lg:gap-8">
                             <a
                                 href="https://laravel.com/docs"
