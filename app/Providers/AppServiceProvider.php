6f4ec380aebc49ec2548b636372aa47fe443c5e9
migbertweb <migbertyanez@disroot.org>
2024-08-08 14:41:43 -0300

    Correccion al AppServiceProvider
    correcion para poder utilizar https en produccion

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,8 +19,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (env('APP_ENV') === 'production') {
-            URL::forceScheme('https'); // or forceSchem('https');
+
+        if($this->app->environment('production')) {
+            \URL::forceScheme('https');
         }
 
     }
