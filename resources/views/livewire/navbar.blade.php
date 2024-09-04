b3be03cffedd4aae9562c488a5046253cf2da65c
migbertweb <migbertyanez@disroot.org>
2024-09-04 15:00:40 -0300

    add estilos, informaciones y boton
    añadido estilos
    añadido boton para descargar curriculo
    cambios al estilo de la pagina

--- resources/views/livewire/navbar.blade.php
+++ resources/views/livewire/navbar.blade.php
@@ -60,27 +60,50 @@
             <div class="relative">
                 <!-- Botón para modo oscuro -->
                 <button id="dark-theme-toggle"
-                    class="bg-gray-800 hover:bg-gray-900 text-white p-3 rounded-full shadow-lg dark:shadow-gray-900 focus:outline-none hidden">
+                    class="bg-gray-800 hover:bg-gray-900 text-white p-2 rounded-full shadow-lg dark:shadow-gray-900 focus:outline-none hidden">
                     <!-- Icono de media luna -->
-                    <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
                         <path
-                            d="M21.752 15.002A9.718 9.718 0 0112.752 22 9.719 9.719 0 013.002 12c0-4.988 3.672-9.127 8.502-9.7a0.75 0.75 0 01.944 0.766A7.253 7.253 0 0012 12a7.253 7.253 0 007.252 7.25a0.75 0.75 0 01.5 1.314z">
+                            d="M8 12a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z">
                         </path>
                     </svg>
                 </button>
 
                 <!-- Botón para modo claro -->
                 <button id="light-theme-toggle"
-                    class="bg-white text-gray-800 hover:bg-gray-300 p-3 rounded-full shadow-xl shadow-gray-500 focus:outline-none">
+                    class="bg-white text-gray-800 hover:bg-gray-300 p-2 rounded-full shadow-xl shadow-gray-500 focus:outline-none">
                     <!-- Icono de foco encendido -->
-                    <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M12 2a7 7 0 00-7 7 7 7 0 003.29 5.852v2.066a1 1 0 001 1h5.42a1 1 0 001-1v-2.066A7 7 0 0019 9a7 7 0 00-7-7zm0 18h1v2a1 1 0 01-1 1h-1a1 1 0 01-1-1v-2h1z">
+                    <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278z">
+                        </path>
+                        <path
+                            d="M10.794 3.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387a1.734 1.734 0 0 0-1.097 1.097l-.387 1.162a.217.217 0 0 1-.412 0l-.387-1.162A1.734 1.734 0 0 0 9.31 6.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387a1.734 1.734 0 0 0 1.097-1.097l.387-1.162zM13.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.156 1.156 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.156 1.156 0 0 0-.732-.732l-.774-.258a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732L13.863.1z">
                         </path>
                     </svg>
                 </button>
             </div>
+
+            <!-- Descargar Curriculo -->
+            <div class="text-center text-gray-800 dark:text-gray-200">
+                <a href="doc/Curriculo.pdf" download>
+                    <button
+                        class="flex text-md items-center p-2 rounded-lg bg-white dark:bg-gray-800 dark:hover:bg-gray-900 dark:text-white text-gray-800 hover:bg-gray-300 shadow-xl shadow-gray-500 dark:shadow-gray-900 focus:outline-none">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" class="w-6 h-6">
+                            <path d="M2 2v60h60V2H2zm58 58H4V4h56v56z" fill="currentColor" />
+                            <path
+                                d="M46 8H18c-1.1 0-2 .9-2 2v44c0 1.1.9 2 2 2h28c1.1 0 2-.9 2-2V10c0-1.1-.9-2-2-2zm0 44H18V10h28v42z"
+                                fill="currentColor" />
+                            <path d="M22 14h20v4H22zm0 8h20v4H22zm0 8h20v4H22zm0 8h20v4H22zm0 8h20v4H22z"
+                                fill="currentColor" />
+                        </svg>
+                        <div class="px-1">
+                            {{ __('resume') }}
+                        </div>
+                    </button>
+                </a>
+            </div>
+
         </div>
     </div>
 </div>
