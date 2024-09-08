c708f29569fc25625225cd102ed98388667db713
migbertweb <migbertyanez@disroot.org>
2024-09-08 15:32:46 -0300

    feat (secciones)
    agregado de contenido a las secciones y modificcacion del sidebar

--- resources/views/livewire/experience.blade.php
+++ resources/views/livewire/experience.blade.php
@@ -1,64 +1,77 @@
-<div class="text-center p-6 lg:py-14">
+<div class="text-center p-6 lg:py-6">
     <div>
         <h3 class="text-3xl py-1 dark:text-white ">{{ __('about_me') }}</h3>
-        <p class="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-            Since the beginning of my journey as a freelance designer and
-            developer, I've done remote work for
-            <span class="text-teal-500">agencies</span>
-            consulted for <span class="text-teal-500">startups</span>
-            and collaborated with talanted people to create digital products
-            for both business and consumer use.
-        </p>
-        <p class="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-            I offer from a wide range of services, including brand design,
-            programming and teaching.
+        <p class="text-lg py-2 leading-8 text-gray-800 dark:text-gray-200">
+            {{ __('bio') }}
         </p>
     </div>
-    <div class="lg:flex gap-6">
-        <div class="text-center shadow-lg shadow-ocean-main p-4 my-6 rounded-xl dark:bg-white flex-1">
+    <div class="lg:flex gap-4">
+        <div
+            class="text-center flex flex-col items-center shadow-lg shadow-ocean-main pt-2 pb-6 my-6 rounded-xl dark:bg-white flex-1">
             <div clas="text-center">
-                <img src="{{ asset('images/misc/design.png') }}" class="h-48 w-48">
+                <img src="{{ asset('images/misc/coding.png') }}" class="h-48 w-48">
             </div>
-            <h3 class="text-lg font-medium pt-8 pb-2  ">
-                Beautiful Designs
+            <h3 class="text-xl font-extrabold pt-6 pb-2  ">
+                {{ __('coding') }}
             </h3>
             <p class="py-2">
-                Creating elegant designs suited for your needs following core
-                design theory.
+                {{ __('coding_desc') }}
             </p>
-            <h4 class="py-4 text-teal-600">Design Tools I Use</h4>
-            <p class="text-gray-800 py-1">Photoshop</p>
-            <p class="text-gray-800 py-1">Illustrator</p>
-            <p class="text-gray-800 py-1">Figma</p>
-            <p class="text-gray-800 py-1">Indesign</p>
+            <h4 class="py-3 text-lg text-bold text-everest-lightest">{{ __('coding_uses') }}</h4>
+            <ul class="list-none">
+                <li class="flex items-center">
+                    <i class="fa-brands fa-php text-gray-900 mr-2"></i>
+                    <span class="text-gray-700">PHP y MySql</span>
+                </li>
+                <li class="flex items-center">
+                    <i class="fa-brands fa-laravel text-gray-900 mr-2"></i>
+                    <span class="text-gray-700">Laravel</span>
+                </li>
+                <li class="flex items-center">
+                    <i class="fa-brands fa-git-alt text-gray-900 mr-2"></i>
+                    <span class="text-gray-700">Git y Github</span>
+                </li>
+                <li class="flex items-center">
+                    <i class="fa-brands fa-docker text-gray-900 mr-2"></i>
+                    <span class="text-gray-700">Docker</span>
+                </li>
+                <li class="flex items-center">
+                    <i class="fa-brands fa-linux text-gray-900 mr-2"></i>
+                    <span class="text-gray-700">GNU Linux</span>
+                </li>
+            </ul>
+
         </div>
-        <div class="text-center shadow-lg shadow-ocean-main p-4 my-6 rounded-xl dark:bg-white flex-1">
-            <img src={{ asset('images/misc/code.png') }} class="h-48 w-48" />
-            <h3 class="text-lg font-medium pt-8 pb-2 ">
-                Code your dream project
+        <div
+            class="text-center flex flex-col items-center shadow-lg shadow-ocean-main pt-2 pb-6 my-6 rounded-xl dark:bg-white flex-1">
+            <div clas="text-center">
+                <img src="{{ asset('images/misc/aptitud.webp') }}" class="h-48 w-48">
+            </div>
+            <h3 class="text-xl font-extrabold pt-6 pb-2  ">
+                {{ __('aptitud') }}
             </h3>
             <p class="py-2">
-                Do you have an idea for your next great website? Let's make it a
-                reality.
+                {{ __('aptitud_desc') }}
             </p>
-            <h4 class="py-4 text-teal-600">Design Tools I Use</h4>
-            <p class="text-gray-800 py-1">Photoshop</p>
-            <p class="text-gray-800 py-1">Illustrator</p>
-            <p class="text-gray-800 py-1">Figma</p>
-            <p class="text-gray-800 py-1">Indesign</p>
-        </div>
-        <div class="text-center shadow-lg shadow-ocean-main p-4 my-6 rounded-xl dark:bg-white flex-1">
-            <img src={{ asset('images/misc/consulting.png') }} class="h-48 w-48" />
-            <h3 class="text-lg font-medium pt-8 pb-2 ">Consulting</h3>
-            <p class="py-2">
-                Are you interested in feedback for your current project? I can
-                give you tips and tricks to level it up.
-            </p>
-            <h4 class="py-4 text-teal-600">Design Tools I Use</h4>
-            <p class="text-gray-800 py-1">Photoshop</p>
-            <p class="text-gray-800 py-1">Illustrator</p>
-            <p class="text-gray-800 py-1">Figma</p>
-            <p class="text-gray-800 py-1">Indesign</p>
+            <h4 class="py-3 text-lg text-bold text-everest-lightest">{{ __('aptitud_uses') }}</h4>
+            <ul class="list-none">
+                <li class="flex items-center">
+                    <i class="fa-solid fa-user-group text-gray-900 mr-2"></i>
+                    <span class="text-gray-700">{{ __('workteam') }}</span>
+                </li>
+                <li class="flex items-center">
+                    <i class="fa-solid fa-bolt text-gray-900 mr-2"></i>
+                    <span class="text-gray-700">{{ __('p_activo') }}</span>
+                </li>
+                <li class="flex items-center">
+                    <i class="fa-solid fa-shapes text-gray-900 mr-2"></i>
+                    <span class="text-gray-700">{{ __('eficient') }}</span>
+                </li>
+                <li class="flex items-center">
+                    <i class="fa-solid fa-gauge-high text-gray-900 mr-2"></i>
+                    <span class="text-gray-700">{{ __('speed_a') }}</span>
+                </li>
+            </ul>
         </div>
     </div>
 </div>
