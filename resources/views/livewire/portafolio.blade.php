dd0485b732854e6a44df4d17b485fcaffe07c21d
migbertweb <migbertyanez@disroot.org>
2024-09-30 22:44:29 -0300

    portafolio de proyectos
    creacion e implementacion de componentes para mostrar los proyectos
    modificaciones menores
    subida de imagenes de proyectos

--- resources/views/livewire/portafolio.blade.php
+++ resources/views/livewire/portafolio.blade.php
@@ -1,140 +1,41 @@
 <div class="text-center p-2 lg:py-2">
-    <div>
-        <h3 class="text-5xl font-brotherline py-1 dark:text-white ">{{ __('portafolio') }}</h3>
-    </div>
-    <div class="lg:flex gap-4">
-        <div
-            class="text-center flex flex-col items-center shadow-lg shadow-ocean-main p-2 pb-6 my-6 rounded-xl bg-white dark:bg-white flex-1">
-            <div clas="text-center">
-                <img src="{{ asset('images/misc/coding.png') }}" class="h-48 w-48">
-            </div>
-            <h3 class="text-xl font-extrabold pt-6 pb-2  ">
-                {{ __('coding') }}
-            </h3>
-            <p class="py-2">
-                {{ __('coding_desc') }}
-            </p>
-            <h4 class="py-3 text-lg text-bold text-everest-lightest">{{ __('coding_uses') }}</h4>
-            <ul class="list-none">
-                <li class="flex items-center">
-                    <i class="fa-brands fa-php text-gray-900 mr-2"></i>
-                    <span class="text-gray-700">PHP y MySql</span>
-                </li>
-                <li class="flex items-center">
-                    <i class="fa-brands fa-laravel text-gray-900 mr-2"></i>
-                    <span class="text-gray-700">Laravel</span>
-                </li>
-                <li class="flex items-center">
-                </li>
-                <li class="flex items-center">
-                    <i class="fa-brands fa-docker text-gray-900 mr-2"></i>
-                    <span class="text-gray-700">Docker</span>
-                </li>
-                <li class="flex items-center">
-                    <i class="fa-brands fa-linux text-gray-900 mr-2"></i>
-                    <span class="text-gray-700">GNU Linux</span>
-                </li>
-            </ul>
 
-        </div>
-        <div
-            class="text-center flex flex-col items-center shadow-lg shadow-ocean-main p-2 pb-6 my-6 rounded-xl bg-white dark:bg-white flex-1">
-            <div clas="text-center">
-                <img src="{{ asset('images/misc/aptitud.webp') }}" class="h-48 w-48">
-            </div>
-            <h3 class="text-xl font-extrabold pt-6 pb-2  ">
-                {{ __('aptitud') }}
-            </h3>
-            <p class="py-2">
-                {{ __('aptitud_desc') }}
-            </p>
-            <h4 class="py-3 text-lg text-bold text-everest-lightest">{{ __('aptitud_uses') }}</h4>
-            <ul class="list-none">
-                <li class="flex items-center">
-                    <i class="fa-solid fa-user-group text-gray-900 mr-2"></i>
-                    <span class="text-gray-700">{{ __('workteam') }}</span>
-                </li>
-                <li class="flex items-center">
-                    <i class="fa-solid fa-bolt text-gray-900 mr-2"></i>
-                    <span class="text-gray-700">{{ __('p_activo') }}</span>
-                </li>
-                <li class="flex items-center">
-                    <i class="fa-solid fa-shapes text-gray-900 mr-2"></i>
-                    <span class="text-gray-700">{{ __('eficient') }}</span>
-                </li>
-                <li class="flex items-center">
-                    <i class="fa-solid fa-gauge-high text-gray-900 mr-2"></i>
-                    <span class="text-gray-700">{{ __('speed_a') }}</span>
-                </li>
-            </ul>
-        </div>
+    <div class="lg:flex gap-4">
+
+        <livewire:project-card title="Mi Portafolio" description="Mi portafolio personal, desarrollado en Laravel"
+            :images="['proyecto1/1.png', 'proyecto1/2.png', 'proyecto1/3.png', 'proyecto1/4.png']" github="https://github.com/migbertweb/portafolio" website="https://migbertweb.site" />
+
+        <livewire:project-card title="Sistema de Gestion de Documentos del Instituto Universitario del Estado Bolivar"
+            description="Aplicativo web desarrollado para la gestion de documentos que un depenedencia de la universidad entrega a los alumnos de manera regular, fue desarrollado en PHP basico con MySql"
+            :images="[
+                'proyecto2/1.jpg',
+                'proyecto2/2.jpg',
+                'proyecto2/3.jpg',
+                'proyecto2/4.jpg',
+                'proyecto2/5.jpg',
+                'proyecto2/6.jpg',
+                'proyecto2/7.jpg',
+            ]" github="https://github.com/migbertweb/sisgdupt" />
+
     </div>
     {{-- segunda seccion --}}
     <div class="lg:flex gap-4">
-        <div
-            class="text-center flex flex-col items-center shadow-lg shadow-ocean-main p-2 pb-6 my-6 rounded-xl bg-white dark:bg-white flex-1">
-            <div clas="text-center">
-                <img src="{{ asset('images/misc/coding.png') }}" class="h-48 w-48">
-            </div>
-            <h3 class="text-xl font-extrabold pt-6 pb-2  ">
-                {{ __('coding') }}
-            </h3>
-            <p class="py-2">
-                {{ __('coding_desc') }}
-            </p>
-            <h4 class="py-3 text-lg text-bold text-everest-lightest">{{ __('coding_uses') }}</h4>
-            <ul class="list-none">
-                <li class="flex items-center">
-                    <i class="fa-brands fa-php text-gray-900 mr-2"></i>
-                    <span class="text-gray-700">PHP y MySql</span>
-                </li>
-                <li class="flex items-center">
-                    <i class="fa-brands fa-laravel text-gray-900 mr-2"></i>
-                    <span class="text-gray-700">Laravel</span>
-                </li>
-                <li class="flex items-center">
-                </li>
-                <li class="flex items-center">
-                    <i class="fa-brands fa-docker text-gray-900 mr-2"></i>
-                    <span class="text-gray-700">Docker</span>
-                </li>
-                <li class="flex items-center">
-                    <i class="fa-brands fa-linux text-gray-900 mr-2"></i>
-                    <span class="text-gray-700">GNU Linux</span>
-                </li>
-            </ul>
 
-        </div>
-        <div
-            class="text-center flex flex-col items-center shadow-lg shadow-ocean-main p-2 pb-6 my-6 rounded-xl bg-white dark:bg-white flex-1">
-            <div clas="text-center">
-                <img src="{{ asset('images/misc/aptitud.webp') }}" class="h-48 w-48">
-            </div>
-            <h3 class="text-xl font-extrabold pt-6 pb-2  ">
-                {{ __('aptitud') }}
-            </h3>
-            <p class="py-2">
-                {{ __('aptitud_desc') }}
-            </p>
-            <h4 class="py-3 text-lg text-bold text-everest-lightest">{{ __('aptitud_uses') }}</h4>
-            <ul class="list-none">
-                <li class="flex items-center">
-                    <i class="fa-solid fa-user-group text-gray-900 mr-2"></i>
-                    <span class="text-gray-700">{{ __('workteam') }}</span>
-                </li>
-                <li class="flex items-center">
-                    <i class="fa-solid fa-bolt text-gray-900 mr-2"></i>
-                    <span class="text-gray-700">{{ __('p_activo') }}</span>
-                </li>
-                <li class="flex items-center">
-                    <i class="fa-solid fa-shapes text-gray-900 mr-2"></i>
-                    <span class="text-gray-700">{{ __('eficient') }}</span>
-                </li>
-                <li class="flex items-center">
-                    <i class="fa-solid fa-gauge-high text-gray-900 mr-2"></i>
-                    <span class="text-gray-700">{{ __('speed_a') }}</span>
-                </li>
-            </ul>
-        </div>
+        <livewire:project-card title="Practica en en Django"
+            description="Creacion de una aplicacion en Django con un frontend en React para gestion de tareas.. simple pero aprendi mucho"
+            :images="['proyecto3/1.png', 'proyecto3/2.png', 'proyecto3/3.png', 'proyecto3/4.png', 'proyecto3/5.png', 'proyecto3/6.png']" github="https://github.com/migbertweb/django_crud_react" />
+
+        <livewire:project-card title="Portafolio en github.io"
+            description="Mi primer intento de realizar un portafolio utilizando mi repositorio estatico en Github"
+            :images="[
+                'proyecto4/1.png',
+                'proyecto4/2.png',
+                'proyecto4/3.png',
+                'proyecto4/4.png',
+                'proyecto4/5.png',
+                'proyecto4/6.png',
+            ]" github="https://github.com/migbertweb/migbertweb.github.io" />
+
     </div>
+    <x-footer-portafolio />
 </div>
