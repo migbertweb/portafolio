36f808ed86ab0fc5a5a5575218638fc75a8b0a58
migbertweb <migbertyanez@disroot.org>
2024-11-24 23:09:09 -0300

    curriculo

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace Tests\Feature;
+
 it('returns a successful response', function () {
     $response = $this->get('/');
 
--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace Tests\Unit;
+
 test('that true is true', function () {
     expect(true)->toBeTrue();
 });
